d264c73bca906f4b9f157e3f8424718d44921981
link-coala <mpj183@aol.com>
2016-05-22 09:30:14 -0500

    header svg on rogth place

--- js/index.js
+++ js/index.js
@@ -16,8 +16,6 @@ var obj = JSON.parse(text);
 
 document.getElementById("demo").innerHTML =
 obj.last ;
-
-
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -35,11 +33,9 @@ obj.last ;
         }
     }
 }
-
 function show(id, value) {
     document.getElementById(id).style.display = value ? 'block' : 'none';
 }
-
 onReady(function () {
     show('page', true);
     show('loading', false);
